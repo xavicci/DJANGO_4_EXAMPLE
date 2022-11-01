fda74734d78ea544f20b61b51c0a59d298c483e8
Xavi Flores <xa_franc@live.com>
2022-10-31 21:53:39 -0600

    conti2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
 def post_list(request):
