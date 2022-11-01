161493b89492a9fc4cc276c2ebb8697cb3ded924
Xavi Flores <xa_franc@live.com>
2022-10-31 22:53:23 -0600

    conti2v1

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator=Paginator(post_list,3)
     page_number=request.GET.get('page',1)
-    posts=paginator.page(page_number)
+    try:
+        posts=paginator.page(page_number)
+    except PageNotAnInteger:
+        posts=paginator.page(1)
+    except EmptyPage:
+        posts=paginator.page(paginator.num_pages)
     return render(request,
                  'blog/post/list.html',
                  {'posts': posts})
-
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -23,3 +28,9 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post}) 
+
+class PostListView(ListView):
+    queryset= Post.published.all()
+    context_object_name= 'posts'
+    paginate_by= 3
+    template_name= 'blog/post/list.html'
