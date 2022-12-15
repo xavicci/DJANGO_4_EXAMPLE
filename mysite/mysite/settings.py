12be993f3b57a8a6b14981829e0c13c42f023368
Xavi Flores <xa_franc@live.com>
2022-12-14 20:40:19 -0600

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w+$@3v*a=ytk-jg-6$lxjnva8q+@$v05_)go)_-#b2ybokilzq'
+SECRET_KEY = 'django-insecure-w+$@3v*a=ytk-jg-6$lxjnva8q+@$v05_)go)_-#b2ybokilzq,djapjqwuwxruckyz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,6 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'xavier.flores.gis@gmail.com'
-EMAIL_HOST_PASSWORD = 'djapjqwuwxruckyz'
+EMAIL_HOST_PASSWORD = 'XXXXX'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
